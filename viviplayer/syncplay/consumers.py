c2366d7ba2f784d8288b264575e1415e807dbb92
jedi <git@m.j3d1.de>
2021-10-27 14:29:51 +0200

    [DEMO] feasibility study for playing synchronous html5 videos for multiple users

---
+++ viviplayer/syncplay/consumers.py
@@ -0,0 +1,49 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        action = text_data_json['action']
+        time = text_data_json['time']
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'action': action,
+                'time': time
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        action = event['action']
+        time = event['time']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'action': action,
+            'time': time
+        }))
