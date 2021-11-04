867e73ab16dbfb350d7ae6b47a295ce3460c428e
The Anh Tran <theanh0226@gmail.com>
2021-11-04 12:39:32 +0100

    remove Rate component

--- viviplayer-frontend/src/App.js
+++ viviplayer-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from "react";
-import { Button, Rate } from "antd";
+import { Button } from "antd";
 import Segment from "./Segment";
 import io from "socket.io-client";
 const markersDefault = [
