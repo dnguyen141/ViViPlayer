6ba61119c4ff527c1d75becbc45449bb748f9497
The Anh Tran <theanh0226@gmail.com>
2021-11-23 23:46:53 +0100

    adding socket server

---
+++ viviplayer-frontend/backend/index.js
@@ -0,0 +1,21 @@
+const { httpServer } = require('socket.io');
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://192.168.2.143:3000',
+    methods: ['GET', 'POST']
+  }
+});
+
+io.listen(5000);
+
+io.on('connection', (socket) => {
+  console.log('MAKE CONNECT TO SOCKER', socket.id);
+  socket.on('playVideo', () => {
+    io.emit('getCommandToPlayVideo');
+  });
+  socket.on('pauseVideo', () => {
+    io.emit('getCommandToPauseVideo');
+  });
+});
+console.log('Running 5000');
