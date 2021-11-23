f41b521a261d6dc05d6ca9ef7c10aed7e55eef14
The Anh Tran <theanh0226@gmail.com>
2021-11-23 23:39:29 +0100

    adding video component and play() pause() function

--- viviplayer-frontend/pages/video/index.js
+++ viviplayer-frontend/pages/video/index.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'antd';
 import Vivilayout from '../../layout/index';
 
-const Video = (props) => {
+const Video = () => {
+  const videoRef = React.useRef(null);
+  const [player, setPlayer] = useState(null);
+  useEffect(() => {
+    if (videoRef.current != null) {
+      setPlayer(videoRef.current);
+    }
+    return () => {};
+  }, [videoRef]);
+  const playVideo = (video) => {
+    console.log('video play');
+    video.play();
+  };
+  const pauseVideo = (video) => {
+    console.log('PAUSE');
+    video.pause();
+  };
   return (
     <Vivilayout>
       <h3>Video</h3>
+      <video
+        // onProgress={(e) => pauseSegment(e)}
+        ref={videoRef}
+        id="video-viviplayer"
+        controls
+        preload="none"
+        width="600"
+        height="400"
+        className="video-js vjs-default-skin"
+      >
+        <source
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <div>
+        <Button type="primary" onClick={() => playVideo(player)}>
+          Play video
+        </Button>
+        <Button type="danger" onClick={() => pauseVideo(player)}>
+          Pause video
+        </Button>
+      </div>
     </Vivilayout>
   );
 };
