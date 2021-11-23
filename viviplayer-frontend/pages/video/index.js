b98fa2efa845102db574cdb259115d4b8cdb6e76
The Anh Tran <theanh0226@gmail.com>
2021-11-23 23:08:35 +0100

    add layout base for project

---
+++ viviplayer-frontend/pages/video/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Vivilayout from '../../layout/index';
+
+const Video = (props) => {
+  return (
+    <Vivilayout>
+      <h3>Video</h3>
+    </Vivilayout>
+  );
+};
+
+Video.propTypes = {};
+
+export default Video;
