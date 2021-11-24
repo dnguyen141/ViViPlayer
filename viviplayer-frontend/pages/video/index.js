2dde0677256ddcf5130e0db7f272f474c21ff133
The Anh Tran <theanh0226@gmail.com>
2021-11-24 17:45:29 +0100

    add markers for video

--- viviplayer-frontend/pages/video/index.js
+++ viviplayer-frontend/pages/video/index.js
@@ -5,6 +5,32 @@ import videoJs from 'video.js';
 import { Button } from 'antd';
 import Vivilayout from '../../layout/index';
 let socket;
+const markersDefault = [
+  {
+    time: 4,
+    text: 'Chapter 1'
+  },
+  {
+    time: 10,
+    text: 'Chapter 2'
+  },
+  {
+    time: 23.6,
+    text: 'Chapter 3'
+  },
+  {
+    time: 50,
+    text: 'Chapter 4'
+  },
+  {
+    time: 136,
+    text: 'Chapter 5'
+  },
+  {
+    time: 226,
+    text: 'Chapter 6'
+  }
+];
 const Video = () => {
   socket = io('http://localhost:5000');
   const videoRef = React.useRef(null);
@@ -21,6 +47,31 @@ const Video = () => {
       const player = (playerRef.current = videoJs(videoElement, () => {
         console.log('player is ready');
       }));
+      // add markers
+      player.markers({
+        markerStyle: {
+          width: '8px',
+          'background-color': 'red'
+        },
+        markerTip: {
+          display: true,
+          html: function (marker) {
+            return <EditableMarkers text={marker.text} />;
+          }
+        },
+        breakOverlay: {
+          display: true,
+          displayTime: 3,
+          text: function (marker) {
+            return marker.text;
+          }
+        },
+        onMarkerReached: function (marker) {
+          console.log(marker);
+          // player.pause();
+        },
+        markers: markersDefault
+      });
       player.autoplay('muted');
       // player.pause();
       pauseVideo(player);
