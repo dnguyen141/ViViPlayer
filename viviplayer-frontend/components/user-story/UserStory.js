3673c872f55f042ead05a4feb13695dfc1728f58
The Anh Tran <theanh0226@gmail.com>
2021-11-28 18:39:52 +0100

    remove unnecessary code

--- viviplayer-frontend/components/user-story/UserStory.js
+++ viviplayer-frontend/components/user-story/UserStory.js
@@ -1,100 +1,102 @@
 import React, { useState } from 'react';
-import { Form, Card, Input, Row, Col, Button } from 'antd';
+import { Form, Card, Input, Button } from 'antd';
 import PropTypes from 'prop-types';
 import styles from './user-story.module.css';
 
 const { TextArea } = Input;
 const tabList = [
-    {
-        key: 'tab1',
-        tab: 'User Story'
-    },
-    {
-        key: 'tab2',
-        tab: 'Satz'
-    },
-    {
-        key: 'tab3',
-        tab: 'Kommentar'
-    }
+  {
+    key: 'tab1',
+    tab: 'User Story'
+  },
+  {
+    key: 'tab2',
+    tab: 'Satz'
+  },
+  {
+    key: 'tab3',
+    tab: 'Kommentar'
+  }
 ];
 
 const UserStory = (props) => {
-    const [activeTabKey1, setActiveTabKey1] = useState('tab1');
-    const onTab1Change = (key) => {
-        setActiveTabKey1(key);
-    };
-    const [form] = Form.useForm();
+  const [activeTabKey1, setActiveTabKey1] = useState('tab1');
+  const onTab1Change = (key) => {
+    setActiveTabKey1(key);
+  };
+  const [form] = Form.useForm();
 
-    const onReset = () => {
-        form.resetFields();
-    };
+  const onReset = () => {
+    form.resetFields();
+  };
 
-    const contentList = {
-        tab1: (
-            <Card title="User Story" style={{ border: '3px solid gray' }}>
-                <h4>Geben Sie ihre User Story ein.</h4>
-                <Form form={form} name="control-hooks" style={{ padding: '5px' }}>
-                    <Form.Item name="label">
-                        <div >
-                            <div style={{ paddingTop: '0.4em' }}>Damit</div>
-                            <div><Input className={styles.inputuser} /></div>
-                        </div>
-                        <div>
-                            <div style={{ paddingTop: '0.4em' }}>
-                                möchte ich als
-                            </div>
-                            <div><Input className={styles.inputuser} /></div>
-                        </div>
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" style={{ margin: '5px', fontSize: '14px' }} htmlType="submit">
-                            Posten
-                        </Button>
-                        <Button htmlType="button" style={{ margin: '5px', fontSize: '14px' }} onClick={onReset}>
-                            Zurücksetzen
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Card>
-        ),
-        tab2: 'This is the Description for User Story and Kommentar.',
-        tab3: (
-            <Card title="Kommentar" style={{ border: '3px solid gray' }}>
-                <h4>Geben Sie ihren Kommentar ein.</h4>
-                <Form form={form} name="control-hooks" style={{ padding: '5px' }}>
-                    <Form.Item name="label">
-                        <div>
-                            <div><TextArea rows={5} style={{ paddingTop: '0.3em' }} /></div>
-                        </div>
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" style={{ margin: '5px', fontSize: '14px' }} htmlType="submit">
-                            Posten
-                        </Button>
-                        <Button htmlType="button" style={{ margin: '5px', fontSize: '14px' }} onClick={onReset}>
-                            Zurücksetzen
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Card>
-        )
-    };
+  const contentList = {
+    tab1: (
+      <Card title="User Story" style={{ border: '3px solid gray' }}>
+        <h4>Geben Sie ihre User Story ein.</h4>
+        <Form form={form} name="control-hooks" style={{ padding: '5px' }}>
+          <Form.Item name="label">
+            <div>
+              <div style={{ paddingTop: '0.4em' }}>Damit</div>
+              <div>
+                <Input className={styles.inputuser} />
+              </div>
+            </div>
+            <div>
+              <div style={{ paddingTop: '0.4em' }}>möchte ich als</div>
+              <div>
+                <Input className={styles.inputuser} />
+              </div>
+            </div>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" style={{ margin: '5px', fontSize: '14px' }} htmlType="submit">
+              Posten
+            </Button>
+            <Button htmlType="button" style={{ margin: '5px', fontSize: '14px' }} onClick={onReset}>
+              Zurücksetzen
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    ),
+    tab2: 'This is the Description for User Story and Kommentar.',
+    tab3: (
+      <Card title="Kommentar" style={{ border: '3px solid gray' }}>
+        <h4>Geben Sie ihren Kommentar ein.</h4>
+        <Form form={form} name="control-hooks" style={{ padding: '5px' }}>
+          <Form.Item name="label">
+            <div>
+              <div>
+                <TextArea rows={5} style={{ paddingTop: '0.3em' }} />
+              </div>
+            </div>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" style={{ margin: '5px', fontSize: '14px' }} htmlType="submit">
+              Posten
+            </Button>
+            <Button htmlType="button" style={{ margin: '5px', fontSize: '14px' }} onClick={onReset}>
+              Zurücksetzen
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    )
+  };
 
-    return (
-        <Col>
-            <Card
-                bordered={false}
-                tabList={tabList}
-                activeTabKey={activeTabKey1}
-                onTabChange={(key) => {
-                    onTab1Change(key);
-                }}
-            >
-                {contentList[activeTabKey1]}
-            </Card>
-        </Col>
-    );
+  return (
+    <Card
+      bordered={false}
+      tabList={tabList}
+      activeTabKey={activeTabKey1}
+      onTabChange={(key) => {
+        onTab1Change(key);
+      }}
+    >
+      {contentList[activeTabKey1]}
+    </Card>
+  );
 };
 
 UserStory.propTypes = {};
