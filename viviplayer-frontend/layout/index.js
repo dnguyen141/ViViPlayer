a8658f741570f9ec8f01be502e4ff9bf276776e6
Matthew <matthew.anfuso@stud.uni-hannover.de>
2021-11-28 12:38:34 +0100

    Layout changes

--- viviplayer-frontend/layout/index.js
+++ viviplayer-frontend/layout/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Router from 'next/router';
 import PropTypes from 'prop-types';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Col } from 'antd';
+
+
+const holder = {
+  xs: { span: 24, offset: 0 },
+  sm: { span: 22, offset: 1 },
+  md: { span: 22, offset: 1 },
+  lg: { span: 20, offset: 2 }
+};
 
 const Vivilayout = ({ children }) => {
   return (
     <Layout>
-      <Layout.Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+      <Layout.Header>
         {/* <div className="logo">Logo there</div> */}
         <Menu theme="dark" mode="horizontal">
           <Menu.Item key="1" className="text-white" onClick={() => Router.push('/dashboard')}>
@@ -18,10 +26,10 @@ const Vivilayout = ({ children }) => {
           <Menu.Item key="3">Session</Menu.Item>
         </Menu>
       </Layout.Header>
-      <Layout.Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
-        <div className="site-layout-background" style={{ padding: 24, minHeight: '80vh' }}>
+      <Layout.Content>
+          <Col {...holder}>
           {children}
-        </div>
+          </Col>
       </Layout.Content>
       <Layout.Footer style={{ textAlign: 'center' }}>Create by Viviplayer3 team</Layout.Footer>
     </Layout>
