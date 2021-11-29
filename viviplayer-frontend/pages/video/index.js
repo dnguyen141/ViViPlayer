bddc4af9c690c2269206e8c814a2b00bb2844ca9
DESKTOP-BQDUELI\pc test <joshua.puglisi@gmail.com>
2021-11-29 20:02:13 +0100

    video player user story flex

--- viviplayer-frontend/pages/video/index.js
+++ viviplayer-frontend/pages/video/index.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 import Video from '../../components/video/Video';
 import UserStory from '../../components/user-story/UserStory';
 import Vivilayout from '../../layout';
+import { Row, Col } from 'antd';
 
 function index(props) {
   return (
     <Vivilayout>
-      <Video />
-      <UserStory />
+      <Row>
+        <Col span={12}>
+          <Video />
+        </Col>
+        <Col span={12}>
+          <UserStory />
+        </Col>
+      </Row>
     </Vivilayout>
   );
 }
