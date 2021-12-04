700c535998788a159a05a2fb1578c373022c11b2
jedi <git@m.j3d1.de>
2021-12-04 11:58:02 +0100

    [DEMO] for frontend reference only. do not merge into main!

--- viviplayer/config/urls.py
+++ viviplayer/config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("authentication.api.urls")),
+    path("test/", include("syncplay.urls"))
 ]
