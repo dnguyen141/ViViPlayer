561225c25bb8b0a69743472f9a4e901e51e36a74
The Anh Tran <theanh0226@gmail.com>
2021-12-10 19:14:09 +0100

    add model schema

---
+++ viviplayer-frontend/backend/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  is_staff: {
+    type: Boolean,
+    default: false
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('user', UserSchema);
