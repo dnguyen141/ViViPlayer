57defbb09d890722f1745a04df1b195cdcafde95
Matthew <matthew.anfuso@stud.uni-hannover.de>
2021-12-11 16:49:04 +0100

    Add session and view related shots

--- viviplayer/session/api/serializers.py
+++ viviplayer/session/api/serializers.py
@@ -4,6 +4,8 @@ from session.models import ViViSession, Shot
 
 class SessionSerializer(serializers.ModelSerializer):
 
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = ViViSession
         fields = '__all__'
@@ -15,11 +17,12 @@ class ShotSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Shot
-        fields = '__all__'
+        fields = ['session', 'title', 'time']
 
+    def to_representation(self, obj):
 
-class FilteredShotSerializer(serializers.ListSerializer):
+        ret = super(ShotSerializer, self).to_representation(obj)
+        ret.pop('session')
 
-    def to_representation(self, data):
-        data = data.filter(session=True)
-        return super(FilteredListSerializer, self).to_representation(data)
+        return ret
+
