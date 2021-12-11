3e1e82c66acbc3660af906f8b0bcaf834b299ad3
Matthew <matthew.anfuso@stud.uni-hannover.de>
2021-12-11 19:54:19 +0100

    Segmentation and Shot creation

--- viviplayer/session/api/urls.py
+++ viviplayer/session/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from session.api.views import ListSessions, ListShots, CreateSession, DetailSession
+from session.api.views import ListSessions, ListShots, CreateSession, DetailSession, CreateShot
 
 # URLs for all authentication views
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('shots/<pk>', ListShots.as_view(), name="rest_shots"),
     path('create', CreateSession.as_view(), name="rest_create_session"),
     path('view/<pk>', DetailSession.as_view(), name="rest_view_session"),
+    path('create_shot/<pk>', CreateShot.as_view(), name="rest_create_shot"),
 ]
