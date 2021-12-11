93444f5281c3e79bcbb36e0584a072f8f6ec0215
Matthew <matthew.anfuso@stud.uni-hannover.de>
2021-12-11 17:31:19 +0100

    Retrieve Session

--- viviplayer/session/api/urls.py
+++ viviplayer/session/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from session.api.views import ListSessions, ListShots, CreateSession
+from session.api.views import ListSessions, ListShots, CreateSession, DetailSession
 
 # URLs for all authentication views
 urlpatterns = [
     path('sessions/', ListSessions.as_view(), name="rest_sessions"),
     path('shots/<pk>', ListShots.as_view(), name="rest_shots"),
     path('create', CreateSession.as_view(), name="rest_create_session"),
+    path('view/<pk>', DetailSession.as_view(), name="rest_view_session"),
 ]
