5e4a85913a7b7fec95cd0b6091ad71c74ca3363d
jedi <git@m.j3d1.de>
2021-12-12 15:58:32 +0100

    fix related names and add migrations

--- viviplayer/session/models.py
+++ viviplayer/session/models.py
@@ -9,29 +9,29 @@ class ViViSessionManager(models.Manager):
 # Create your models here.
 class ViViSession(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False, unique=True)
-    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="owner_ref")
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="sessions")
     current_users = models.ManyToManyField(CustomUser, related_name="current_users", blank=True)
-    tan = models.CharField(min_length=8, max_length=20, null=True, blank=False)
+    tan = models.CharField(max_length=20, null=True, blank=False)
     video_path = models.URLField(max_length=200)
     is_opened = models.BooleanField(default=False)
 
 
 class Shot(models.Model):
-    session = models.ForeignKey(ViViSession, on_delete=models.CASCADE, related_name="session_ref")
+    session = models.ForeignKey(ViViSession, on_delete=models.CASCADE, related_name="shots")
     time = models.FloatField()
     title = models.CharField(max_length=15, null=False, blank=False)
 
 
 class UserStory(models.Model):
-    session = models.ForeignKey(ViViSession, on_delete=models.CASCADE, related_name="session_ref")
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="author_ref")
-    shot = models.ForeignKey(Shot, on_delete=models.CASCADE, related_name="shot_ref")
+    session = models.ForeignKey(ViViSession, on_delete=models.CASCADE, related_name="userstories")
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="userstories")
+    shot = models.ForeignKey(Shot, on_delete=models.CASCADE, related_name="userstories")
     desc = models.CharField(max_length=500, null=False, blank=False)
 
 
 class Sentence(models.Model):
-    session = models.ForeignKey(ViViSession, on_delete=models.CASCADE, related_name="session_ref")
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="author_ref")
+    session = models.ForeignKey(ViViSession, on_delete=models.CASCADE, related_name="sentence")
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="sentence")
     text = models.CharField(max_length=500, null=False, blank=False)
 
 
@@ -41,14 +41,14 @@ class MultipleChoiceQuestion(models.Model):
         ("survey", "Survey"),
     )
 
-    session = models.ForeignKey(ViViSession, on_delete=models.CASCADE, related_name="session_ref")
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="author_ref")
+    session = models.ForeignKey(ViViSession, on_delete=models.CASCADE, related_name="questions")
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="questions")
     desc = models.CharField(max_length=500, null=False, blank=False)
     type = models.CharField(max_length=8, choices=MCQ_TYPE)
 
 
 class Answer(models.Model):
-    question = models.ForeignKey(MultipleChoiceQuestion, on_delete=models.CASCADE, related_name="mcq_ref")
+    question = models.ForeignKey(MultipleChoiceQuestion, on_delete=models.CASCADE, related_name="answers")
     text = models.CharField(max_length=100)
     votes = models.IntegerField(max_length=2)
 
