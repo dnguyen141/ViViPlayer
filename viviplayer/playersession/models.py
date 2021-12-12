83b49402ef807caebde4a5cfb3d119607f2691c7
jedi <git@m.j3d1.de>
2021-12-12 15:58:32 +0100

    fix related names and add migrations

--- viviplayer/playersession/models.py
+++ viviplayer/playersession/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from session.models import ViViSession
+
 
 class UserStory(models.Model):
     desc = models.CharField(max_length=240)
+    session = models.ForeignKey(ViViSession, default=1, verbose_name="Session", on_delete=models.SET_DEFAULT, related_name="userstory")
