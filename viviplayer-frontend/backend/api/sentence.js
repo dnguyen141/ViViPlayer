f24a26a5ee2c613190f9cf1f0b5a7ccc9a0900f6
The Anh Tran <theanh0226@gmail.com>
2021-12-15 14:20:39 +0100

    create, update sentence

--- viviplayer-frontend/backend/api/sentence.js
+++ viviplayer-frontend/backend/api/sentence.js
@@ -24,10 +24,74 @@ router.get('/', auth, async (req, res) => {
 // @desc     create one sentence
 // @access   Public
 router.post('/create-sentence', async (req, res) => {
-  const { owner, sessionId } = req.body;
+  const { owner, sessionId, sentence } = req.body;
   const Owner = await User.findOne({ _id: owner });
+  if (!Owner) {
+    return res.status(400).json({ errors: [{ msg: 'You do not have permission' }] });
+  }
   const sessionRef = await Session.findOne({ _id: sessionId });
-  res.json(sessionRef);
+  if (!sessionRef) {
+    return res.status(400).json({ errors: [{ msg: 'Session not found' }] });
+  }
+  let check = sessionRef.userLists.find((x) => {
+    if (x.user.toString() === Owner._id.toString()) {
+      return true;
+    }
+  });
+  if (!check) {
+    return res.status(400).json({ errors: [{ msg: 'You do not have permission' }] });
+  }
+  try {
+    await Session.findOneAndUpdate(
+      { _id: sessionId },
+      {
+        $push: {
+          sentenceLists: { userName: Owner.name, content: sentence }
+        }
+      },
+      {
+        useFindAndModify: false
+      }
+    );
+    return res.status(200).json({ msg: 'you have successfully written the sentence' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route    Put api/edit-sentence
+// @desc     edit sentence
+// @access   Public
+router.put('/edit-sentence', async (req, res) => {
+  const { owner, sessionId, sentence, sentenceId } = req.body;
+  const Owner = await User.findOne({ _id: owner });
+  if (!Owner) {
+    return res.status(400).json({ errors: [{ msg: 'You do not have permission' }] });
+  }
+  const sessionRef = await Session.findOne({ _id: sessionId });
+  if (!sessionRef) {
+    return res.status(400).json({ errors: [{ msg: 'Session not found' }] });
+  }
+  try {
+    // Pull out sentence
+    const findSentence = sessionRef.sentenceLists.find((sen) => sen.id === sentenceId);
+    // Make sure sentence exists
+    if (!findSentence) {
+      return res.status(404).json({ msg: 'sentence does not exist' });
+    }
+    // update sentence
+    sessionRef.sentenceLists.find((sen) => {
+      if (sen.id === sentenceId) {
+        sen.content = sentence;
+      }
+    });
+    await sessionRef.save();
+    res.json(sessionRef);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
