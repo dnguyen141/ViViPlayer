ff8c39f8cd3858415e3c62c9747ccae29c6bd794
Matthew <matthew.anfuso@stud.uni-hannover.de>
2021-12-17 23:09:56 +0100

    Created MEDIA_URL and MEDIA_ROOT

--- viviplayer/config/urls.py
+++ viviplayer/config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Swagger API UI
 from rest_framework import permissions
@@ -40,4 +42,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("authentication.api.urls")),
     path("api/session/", include("session.api.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
