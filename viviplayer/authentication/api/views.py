b5cdab4946148fee9c8caa190d29af6743618d7a
dinnguyen1495 <minhdinhnguyen1495@gmail.com>
2021-12-18 22:51:36 +0100

    fix error messages

--- viviplayer/authentication/api/views.py
+++ viviplayer/authentication/api/views.py
@@ -26,11 +26,11 @@ def custom_exception_handler(exc, context):
     # Update the structure of the response data.
     if response is not None:
         customized_response = dict()
-        customized_response['errors'] = []
+        customized_response["errors"] = []
 
         for key, value in response.data.items():
-            error = {'field': key, 'message': value[0]}
-            customized_response['errors'].append(error)
+            error = {"field": key, "message": value}
+            customized_response["errors"].append(error)
 
         response.data = customized_response
 
