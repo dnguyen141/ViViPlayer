46f125ad0ef296cbdc9fd8442d34483a12cd8a50
The Anh Tran <theanh0226@gmail.com>
2021-12-20 00:24:38 +0100

    remove video

--- viviplayer-frontend/components/session/SessionForm.js
+++ viviplayer-frontend/components/session/SessionForm.js
@@ -1,18 +1,80 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Form, Input, Row, Col, Button, Upload, Divider } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import axios from 'axios';
 import PropTypes from 'prop-types';
+import api from '../../utils/api';
+import { loadUser } from '../../actions/auth.action';
 
 function SessionForm(props) {
+  // useEffect(() => {
+  //   // check for token in LS when app first runs
+  //   if (localStorage.token) {
+  //     // if there is a token set axios headers for all requests
+  //     // setAuthToken(localStorage.token);
+  //     axios.defaults.headers.common['Authorization'] = `Token ${localStorage.token}`;
+  //   } else {
+  //     Router.push('/');
+  //   }
+  //   // try to fetch a user, if no token or invalid token we
+  //   // will get a 401 response from our API
+  //   loadUser();
+
+  //   // log user out from all tabs if they log out in one tab
+  //   // window.addEventListener('storage', () => {
+  //   //   if (!localStorage.token) {
+  //   //     type: LOGOUT;
+  //   //   }
+  //   // });
+  // }, []);
   const layout = {
     labelCol: { span: 5 },
     wrapperCol: { span: 12 }
   };
+  const inputRef = React.useRef();
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    console.log(file);
+    const formData = new FormData();
+    formData.append('video_path', file);
+    formData.append('name', 'Testiasdasdng');
+    formData.append('tan', 'asdasdasdasd23123');
+    // const url = URL.createObjectURL(file);
 
-  const onFinish = (values) => {
-    console.log('Success:', values);
+    const res = await api.post('/session/create', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
   };
+  const onFinish = async (values) => {
+    const body = {
+      video_path: values.video,
+      name: values.name,
+      tan: values.tan
+    };
+    // console.log('BODY', body);
+    console.log('VIDEO', values.video.file);
+    const formData = new FormData();
+    formData.append('video_path', values.video.file);
+    formData.append('name', values.name);
+    formData.append('tan', values.name);
+    console.log('FORMDATA', formData);
+    // console.log('Success:', values);
+    // console.log('video_path:', values.video.file);
+    // console.log('name:', values.name);
+    // console.log('tan:', values.tan);
 
+    const res = await api.post('/session/create', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+    console.log(res.response);
+  };
+  const handleChoose = (event) => {
+    inputRef.current.click();
+  };
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
@@ -38,6 +100,14 @@ function SessionForm(props) {
       <Col span={11}>
         <h1>Session Erstellen</h1>
         <Divider />
+        <input
+          ref={inputRef}
+          className="VideoInput_input"
+          type="file"
+          onChange={handleFileChange}
+          accept=".mov,.mp4"
+        />
+        <button onClick={handleChoose}>Choose</button>
         <Form
           {...layout}
           name="basic"
@@ -56,7 +126,7 @@ function SessionForm(props) {
           </Form.Item>
           <Form.Item
             label="Session Name"
-            name="sessionname"
+            name="name"
             rules={[{ required: true, message: 'Name der Session kann nicht Leer sein!' }]}
           >
             <Input placeholder="Geben Sie hier Namen Ihrer Session ein." />
