ec37f75304f9ede87a77c0b90218f2b592104ab4
dinnguyen1495 <minhdinhnguyen1495@gmail.com>
2021-12-20 22:31:31 +0100

    Clean up unused libraries and codes

--- viviplayer/session/api/views.py
+++ viviplayer/session/api/views.py
@@ -1,16 +1,12 @@
 from rest_framework import generics, permissions
 from session.models import ViViSession, Shot, UserStory, Sentence, MultipleChoiceQuestion
-from session.api.serializers import SessionSerializer, ShotSerializer, CreateShotSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 from authentication.permissions import IsModerator
-from rest_framework import viewsets, mixins
-from authentication.permissions import IsModerator
-from .serializers import SessionSerializer, UserStorySerializer, SentenceSerializer, QuestionSerializer
+from .serializers import SessionSerializer, UserStorySerializer, SentenceSerializer, QuestionSerializer, ShotSerializer
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework import status
 from rest_framework.response import Response
-from session.permissions import IsAuthorOrReadOnly
-
 
 # Create your views here.
 class SessionViewSet(viewsets.ModelViewSet):
@@ -39,26 +35,6 @@ class SessionViewSet(viewsets.ModelViewSet):
             }
             return Response(error, status=status.HTTP_403_FORBIDDEN, headers={})
         return super().create(request, *args, **kwargs)
-
-
-# class ListShots(generics.ListAPIView):
-#     serializer_class = ShotSerializer
-#     queryset = Shot.objects.all()
-#     permission_classes = [IsAuthenticated]
-#
-#     # def get_queryset(self):
-#     #     ses = self.kwargs['pk']
-#     #     return Shot.objects.filter(session_id=ses)
-#
-#
-# class CreateShot(generics.CreateAPIView):
-#     serializer_class = CreateShotSerializer
-#     queryset = Shot.objects.all()
-#     permission_classes = [IsModerator]
-#
-#     def perform_create(self, serializer):
-#         ses = ViViSession.objects.get(id=self.kwargs['pk'])
-#         serializer.save(session=ses)
 
 
 # Update and Destroy Shots
