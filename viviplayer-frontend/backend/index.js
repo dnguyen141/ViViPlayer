1bc6f6f8063dc4e38d576417b74d663ff6b45fec
DESKTOP-BQDUELI\pc test <joshua.puglisi@gmail.com>
2021-12-21 18:01:56 +0100

    progress

--- viviplayer-frontend/backend/index.js
+++ viviplayer-frontend/backend/index.js
@@ -2,7 +2,7 @@ const { httpServer } = require('socket.io');
 
 const io = require('socket.io')(httpServer, {
   cors: {
-    origin: 'http://192.168.2.143:3000',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST']
   }
 });
--- viviplayer-frontend/components/satz/Satz.js
+++ viviplayer-frontend/components/satz/Satz.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Comment, Form, Button, List, Input } from 'antd';
+import api from '../../utils/api';
 
 const Satz = (props) => {
   const [comments, setComments] = useState([]);
   const [submitting, setSubmitting] = useState(false);
   const [value, setValue] = useState('');
   const { TextArea } = Input;
+  getSentence();
   const CommentList = ({ comments }) => (
     <List
       dataSource={comments}
@@ -15,11 +17,30 @@ const Satz = (props) => {
     />
   );
 
-  const handleSubmit = () => {
+  async function getSentence (){
+      const req = await api.get('/session/sentences/');
+      console.log(req.data);
+      setTimeout(getSentence, 1000);
+  }
+
+  const onFinish = async (values) => {
+    const formData = new FormData();
+    formData.append('text', values.sentence);
+    formData.append('shot', 1);
+    const res = await api.post('/session/sentences/', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+    console.log(res.data);
+  }
+
+  const handleSubmit = async () => {
     if (!value) {
       console.log('run there');
       return;
     }
+    
     setSubmitting(true);
     setTimeout(() => {
       setSubmitting(false);
@@ -33,15 +54,18 @@ const Satz = (props) => {
       ]);
       console.log(comments);
     }, 1000);
+
+    
   };
 
   return (
     <>
       {comments != undefined && <CommentList comments={comments} />}
       <Comment
+        onFinish={onFinish}
         content={
           <>
-            <Form.Item>
+            <Form.Item name="sentence">
               <TextArea rows={3} onChange={(e) => setValue(e.target.value)} value={value} placeholder="Bitte geben Sie Ihren Kommentar hier ein" />
             </Form.Item>
             <Form.Item>
