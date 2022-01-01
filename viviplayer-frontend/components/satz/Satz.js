3278c6c6ffe2641c2a2d394859eb3ccb29246057
The Anh Tran <theanh0226@gmail.com>
2022-01-01 14:24:06 +0100

    added table to render sentences list

--- viviplayer-frontend/components/satz/Satz.js
+++ viviplayer-frontend/components/satz/Satz.js
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Comment, Form, Button, List, Input } from 'antd';
+import {
+  Comment,
+  Form,
+  Button,
+  List,
+  Input,
+  Table,
+  InputNumber,
+  Modal,
+  Space,
+  Popconfirm
+} from 'antd';
 import { getSentences } from '../../actions/session.action';
 import { connect } from 'react-redux';
 const Satz = ({ getSentences, sentences }) => {
   const [comments, setComments] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [updateTable, setupdateTable] = useState(false);
   const [sentencesList, setSentencesList] = useState(null);
   const [submitting, setSubmitting] = useState(false);
   const [value, setValue] = useState('');
@@ -19,8 +32,53 @@ const Satz = ({ getSentences, sentences }) => {
   useEffect(() => {
     getSentences();
     setSentencesList(sentences);
-  }, []);
-  console.log('GET ALL SENTENCES', sentences);
+  }, [updateTable]);
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      render: (id) => <p>{id}</p>
+    },
+    {
+      title: 'Inhalt',
+      dataIndex: 'text',
+      render: (text) => <p>{text}</p>
+    },
+    {
+      title: 'Action',
+      dataIndex: 'id',
+      render: (id) => (
+        <Space size="middle">
+          <Button
+            type="primary"
+            onClick={() => {
+              setIsModalVisible(true);
+              console.log('click', isModalVisible);
+            }}
+          >
+            Edit
+          </Button>
+          <Popconfirm
+            title="Sicher zu löschen kann nicht rückgängig machen?"
+            onConfirm={() => removeGood(_id)}
+          >
+            <Button type="primary" danger>
+              Delete
+            </Button>
+          </Popconfirm>
+          <Modal
+            title="Edit Text"
+            visible={isModalVisible}
+            onOk={() => setIsModalVisible(false)}
+            onCancel={() => setIsModalVisible(false)}
+          >
+            {id}
+          </Modal>
+        </Space>
+      )
+    }
+  ];
   const handleSubmit = () => {
     if (!value) {
       return;
@@ -61,6 +119,19 @@ const Satz = ({ getSentences, sentences }) => {
             </Form.Item>
           </>
         }
+      />
+      <Table
+        className="number-table"
+        pagination={{ pageSize: 5 }}
+        // onRow={(record, rowIndex) => {
+        //   return {
+        //     onClick: (event) => {
+        //       fetchGoodById(goods.data[rowIndex]._id);
+        //     },
+        //   };
+        // }}
+        columns={columns}
+        dataSource={sentencesList}
       />
     </>
   );
