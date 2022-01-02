c30e6a632a338256f8a8ac392acc4c1527213904
dinnguyen1495 <minhdinhnguyen1495@gmail.com>
2022-01-02 16:51:24 +0100

    change userstory's format

--- viviplayer/session/models.py
+++ viviplayer/session/models.py
@@ -12,8 +12,9 @@ class ViViSessionManager(models.Manager):
 # Create your models here.
 class ViViSession(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False, unique=True)
-    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="sessions")
-    current_users = models.ManyToManyField(CustomUser, related_name="current_users", blank=True)
+    owner = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="sessions"
+    )
     tan = models.CharField(max_length=20, blank=False)
     video_path = models.FileField(max_length=200)
     is_opened = models.BooleanField(default=False)
@@ -28,14 +29,16 @@ def segment_video(sender, instance, created, *args, **kwargs):
     if created:
         time_stamps = autosegment.find_scenes(instance.video_path.path, instance.name)
         for i, time in enumerate(time_stamps):
-            shot = Shot(session=instance, title=f'Unbenannter Shot {i + 1}', time=time)
+            shot = Shot(session=instance, title=f"Unbenannter Shot {i + 1}", time=time)
             shot.save()
         instance.segmented = True
         instance.save()
 
 
 class Shot(models.Model):
-    session = models.ForeignKey(ViViSession, on_delete=models.CASCADE, related_name="shots")
+    session = models.ForeignKey(
+        ViViSession, on_delete=models.CASCADE, related_name="shots"
+    )
     time = models.FloatField()
     title = models.CharField(max_length=50, null=False, blank=False)
     image = models.URLField(max_length=200, null=True)
@@ -50,20 +53,32 @@ def get_screenshot(sender, instance, created, *args, **kwargs):
     if created:
         vid = ViViSession.objects.get(shots=instance)
         imageextractor.extract(vid.video_path.path, vid.id, [instance.time])
-        instance.image = 'media/screenshots/' + str(vid.id) + '/' + str(instance.time) + '.jpg'
+        instance.image = (
+            "media/screenshots/" + str(vid.id) + "/" + str(instance.time) + ".jpg"
+        )
         instance.save()
 
 
 class UserStory(models.Model):
-    session = models.ForeignKey(ViViSession, on_delete=models.CASCADE, related_name="userstories")
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="userstories")
+    session = models.ForeignKey(
+        ViViSession, on_delete=models.CASCADE, related_name="userstories"
+    )
+    author = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="userstories"
+    )
     shot = models.ForeignKey(Shot, on_delete=models.CASCADE, related_name="userstories")
-    desc = models.CharField(max_length=500, null=False, blank=False)
+    damit = models.CharField(max_length=500, null=False, blank=False)
+    moechteichals1 = models.CharField(max_length=500, null=False, blank=False)
+    moechteichals2 = models.CharField(max_length=500, null=False, blank=False)
 
 
 class Sentence(models.Model):
-    session = models.ForeignKey(ViViSession, on_delete=models.CASCADE, related_name="sentence")
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="sentence")
+    session = models.ForeignKey(
+        ViViSession, on_delete=models.CASCADE, related_name="sentence"
+    )
+    author = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="sentence"
+    )
     shot = models.ForeignKey(Shot, on_delete=models.CASCADE, related_name="sentence")
     text = models.CharField(max_length=500, null=False, blank=False)
 
@@ -74,14 +89,20 @@ class MultipleChoiceQuestion(models.Model):
         ("survey", "Survey"),
     )
 
-    session = models.ForeignKey(ViViSession, on_delete=models.CASCADE, related_name="questions")
+    session = models.ForeignKey(
+        ViViSession, on_delete=models.CASCADE, related_name="questions"
+    )
     shot = models.ForeignKey(Shot, on_delete=models.CASCADE, related_name="questions")
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="questions")
+    author = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="questions"
+    )
     desc = models.CharField(max_length=500, null=False, blank=False)
     type = models.CharField(max_length=8, choices=MCQ_TYPE)
 
 
 class Answer(models.Model):
-    question = models.ForeignKey(MultipleChoiceQuestion, on_delete=models.CASCADE, related_name="answers")
+    question = models.ForeignKey(
+        MultipleChoiceQuestion, on_delete=models.CASCADE, related_name="answers"
+    )
     text = models.CharField(max_length=100)
     votes = models.IntegerField()
