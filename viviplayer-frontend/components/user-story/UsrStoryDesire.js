3d1d264a294002ca348c517ae91f168f66b4c783
The Anh Tran <theanh0226@gmail.com>
2022-01-03 21:41:31 +0100

    implement create func and get all user story

--- viviplayer-frontend/components/user-story/UsrStoryDesire.js
+++ viviplayer-frontend/components/user-story/UsrStoryDesire.js
@@ -1,124 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Comment, Form, Button, List, Input } from 'antd';
-import styles from './user-story.module.css';
-
-function UsrStoryDesire(props) {
-  const [comments, setComments] = useState([]);
-  const [submitting, setSubmitting] = useState(false);
-  const [value1, setValue1] = useState(''); //value1 for the first text box
-  const [value2, setValue2] = useState(''); //value2 for the second text box
-  const [value3, setValue3] = useState(''); ///value3 for the third text box
-  const CommentList = ({ comments }) => (
-    <List
-      dataSource={comments}
-      itemLayout="horizontal"
-      renderItem={(props) => <Comment {...props} />}
-      className="scroll-bar"
-    />
-  );
-
-  const handleSubmit = () => {
-    if (!value1 || !value2 || !value3) {
-      console.log('run there');
-      return;
+import api from '../../utils/api';
+import { connect } from 'react-redux';
+import { Button, Input, Table, Space, Popconfirm, Form } from 'antd';
+import { createUserStory } from '../../actions/session.action';
+const { TextArea } = Input;
+const UsrStoryDesire = ({ createUserStory, user }) => {
+  const [damit, setDamit] = useState(''); //value1 for the first text box
+  const [moechteichals1, setMoechteichals1] = useState(''); //value2 for the second text box
+  const [moechteichals2, setMoechteichals2] = useState(''); ///value3 for the third text box
+  const [updateTable, setupdateTable] = useState(false);
+  const [userStories, setUserStories] = useState(null);
+  const [form] = Form.useForm();
+  useEffect(() => {
+    async function fetchUserStories() {
+      const res = await api.get('/session/userstories/');
+      setUserStories(res.data);
     }
-    setSubmitting(true);
-    setTimeout(() => {
-      setSubmitting(false);
-      setValue1('');
-      setValue2('');
-      setValue3('');
-      setComments([
-        ...comments,
-        {
-          author: 'User',
-          content: (
-            
-            <p>
-              Damit {value1}, möchte ich als {value2}, {value3}.
-            </p>
-          ),
-        }
-      ]);
-      console.log(comments);
-    }, 1000);
-  };
+    fetchUserStories();
+  }, [updateTable]);
+  // create table
+  const columns = [
+    {
+      title: 'user',
+      width: '15%',
+      render: () => <p>{user.username && <p>{user.username}</p>}</p>
+    },
+    {
+      title: 'Inhalt',
+      dataIndex: 'id',
+      width: '50%',
+      render: (id, record) => (
+        <p>
+          Damit {record.damit},möchte ich als {record.moechteichals1},{record.moechteichals2}
+        </p>
+      )
+    },
+    {
+      title: 'Shot',
+      dataIndex: 'shot',
+      width: '10%',
+      render: (shot) => <p>{shot}</p>
+    },
+    {
+      title: 'Aktionen',
+      dataIndex: 'id',
+      render: (id, record) => (
+        <Space size="middle">
+          {/* <EditSentence id={id} context={record} updateFunc={updateState} /> */}
+          <Popconfirm title="Löschen dieses Satzes ist nicht rückgängig zu machen. Weiter?">
+            <Button type="primary" danger>
+              Delete
+            </Button>
+          </Popconfirm>
+        </Space>
+      )
+    }
+  ];
 
-  const onReset = () => {
-    setValue1('');
-    setValue2('');
-    setValue3('');
+  const createUserStoryFunc = ({ damit, moechteichals1, moechteichals2, shot }) => {
+    createUserStory(damit, moechteichals1, moechteichals2, shot);
+    setupdateTable(!updateTable);
   };
-
   return (
     <>
-      {comments != undefined && <CommentList comments={comments} />}
-      <Comment
-        content={
-          <>
-            <Form>
-              <Form.Item name="label">
-                <div>
-                  <div style={{ paddingTop: '0.4em' }}>Damit</div>
-                  <div>
-                    <Input
-                      className={styles.inputuser}
-                      placeholder='z.B. ich weiß, ob jemand unregelmäßig arbeitet'
-                      value={value1}
-                      onChange={(e) => setValue1(e.target.value)}
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div style={{ paddingTop: '0.4em' }}>möchte ich als</div>
-                  <div>
-                    <Input
-                      className={styles.inputuser}
-                      placeholder='z.B. Abteilungsleiter'
-                      value={value2}
-                      onChange={(e) => setValue2(e.target.value)}
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div style={{ paddingTop: '0.4em' }} />
-                  <div>
-                    <Input
-                      className={styles.inputuser}
-                      placeholder='z.B. eine visuelle Darstellung der geleisteten Stunden sehen'
-                      value={value3}
-                      onChange={(e) => setValue3(e.target.value)}
-                    />
-                  </div>
-                </div>
-              </Form.Item>
-              <Form.Item>
-                <Button
-                  type="primary"
-                  style={{ margin: '5px', fontSize: '14px', marginLeft: '0px', marginBottom: '0px' }}
-                  htmlType="submit"
-                  loading={submitting}
-                  onClick={handleSubmit}
-                >
-                  Posten
-                </Button>
-                <Button
-                  htmlType="button"
-                  style={{ margin: '5px', fontSize: '14px', marginBottom: '0px' }}
-                  onClick={onReset}
-                >
-                  Zurücksetzen
-                </Button>
-              </Form.Item>
-            </Form>
-          </>
-        }
+      <Table
+        className="number-table"
+        columns={columns}
+        pagination={false}
+        dataSource={userStories}
+        scroll={{ y: 200 }}
       />
+      <Form form={form} name="Write sentence" onFinish={createUserStoryFunc} autoComplete="off">
+        Damit :
+        <Form.Item style={{ marginBottom: '1em' }} name="damit">
+          <Input rows={4} placeholder="z.B ich weiss,ob jemand unregelmäßig arbeitet" />
+        </Form.Item>
+        möchte ich als:
+        <Form.Item style={{ marginBottom: '1em' }} name="moechteichals1">
+          <Input rows={4} placeholder="z.B Abteulungsleiter" />
+        </Form.Item>
+        <Form.Item style={{ marginBottom: '1em' }} name="moechteichals2">
+          <Input
+            rows={4}
+            placeholder="z.B eine visuelle Darstellung der geleisteten Stunden sehen"
+          />
+        </Form.Item>
+        <Form.Item style={{ marginBottom: '1em' }} name="shot">
+          <Input placeholder="Geben Sie Shot-Nummer ein." />
+        </Form.Item>
+        <Button type="primary" htmlType="submit">
+          Posten
+        </Button>
+      </Form>
     </>
   );
-}
+};
 
 UsrStoryDesire.propTypes = {};
 
-export default UsrStoryDesire;
+const mapStateToProps = (state) => ({
+  user: state.auth.user
+});
+export default connect(mapStateToProps, { createUserStory })(UsrStoryDesire);
