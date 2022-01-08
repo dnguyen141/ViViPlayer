18da9e2d1552c747c994b5cde8955697448edb54
The Anh Tran <theanh0226@gmail.com>
2022-01-08 23:04:37 +0100

    change layout of dashboard page

--- viviplayer-frontend/pages/dashboard/index.js
+++ viviplayer-frontend/pages/dashboard/index.js
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Vivilayout from '../../layout/index';
 import Router from 'next/router';
+import api from '../../utils/api';
 import SessionForm from '../../components/session/SessionForm';
+import SessionTable from '../../components/session/SessionTable';
 import { loadUser } from '../../actions/auth.action';
 import { setAuthToken } from '../../utils/setAuthToken';
 import { connect } from 'react-redux';
+import { Col, Row, Divider } from 'antd';
 
-const Dashboard = ({ user, loadUser }) => {
+const Dashboard = ({ user, loadUser, loading }) => {
+  const [updateTable, setupdateTable] = useState(false);
+  const [sessionData, setSessionData] = useState([]);
   useEffect(() => {
     // check for token in LS when app first runs
     if (localStorage.token) {
@@ -30,16 +35,39 @@ const Dashboard = ({ user, loadUser }) => {
   if (user && user.is_mod == false) {
     Router.push('/video');
   }
+  async function fetchSession() {
+    const res = await api.get('/session/');
+    console.log(res.data);
+    setSessionData(res.data);
+  }
 
+  useEffect(() => {
+    fetchSession();
+    console.log('run there');
+  }, [updateTable, loading]);
   return (
     <Vivilayout>
-      <SessionForm />
+      <Row>
+        <Col span={12} style={{ padding: '1em' }}>
+          <h3>Session List</h3>
+          <Divider />
+          {sessionData != 0 ? (
+            <SessionTable updateLayoutState={updateTable} updateLayout={setupdateTable} />
+          ) : (
+            'You have no session'
+          )}
+        </Col>
+        <Col span={12} style={{ padding: '1em' }}>
+          <SessionForm updateLayoutState={updateTable} updateLayout={setupdateTable} />
+        </Col>
+      </Row>
     </Vivilayout>
   );
 };
 
 const mapStateToProps = (state) => ({
-  user: state.auth.user
+  user: state.auth.user,
+  loading: state.session.loading
 });
 
 export default connect(mapStateToProps, { loadUser })(Dashboard);
