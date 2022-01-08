7c0da5b4c48a3d6de282f6508b10586c55264c82
The Anh Tran <theanh0226@gmail.com>
2022-01-08 19:00:23 +0100

    add sesssion components

---
+++ viviplayer-frontend/components/session/Session.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SessionForm from './SessionForm';
+import SessionTable from './SessionTable';
+import { Row, Col } from 'antd';
+
+function Session(props) {
+  return <div>1</div>;
+}
+
+Session.propTypes = {};
+
+export default Session;
