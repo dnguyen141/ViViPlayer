94297ad02ddd43a28d4408dfe473dbc2b96abb81
DESKTOP-BQDUELI\pc test <joshua.puglisi@gmail.com>
2022-01-13 00:47:29 +0100

    video player fix

--- viviplayer-frontend/components/video/Video.js
+++ viviplayer-frontend/components/video/Video.js
@@ -67,7 +67,7 @@ const Video = ({ loadUser, loading, user }) => {
   const [autoStop, setAutoStop] = useState(false);
   const [chapterText, setChapterText] = useState('');
   const [lastTime, setLastTime] = useState(0);
-  const [visibleChapterText, setVisibleChapterText] = useState('translateY(-100%)'); // -100% = disappear, 0 = appear
+  const [visibleChapterText, setVisibleChapterText] = useState('hidden'); // -100% = disappear, 0 = appear
 
   const insertArray = async () => {
     var markerListTemp = [];
@@ -137,7 +137,7 @@ const Video = ({ loadUser, loading, user }) => {
     );
 
     if (temp != null) {
-      setVisibleChapterText('translateY(0%)');
+      setVisibleChapterText('visible');
       console.log(lastTime);
       setLastTime(temp.time); //last marker that was found.
       setChapterText(temp.text);
@@ -154,7 +154,7 @@ const Video = ({ loadUser, loading, user }) => {
     requestAnimationFrame(() => {
       //checks whether the chapter title should still be shown
       if (lastTime === -1 || videoRef.current.currentTime > lastTime + 5) {
-        setVisibleChapterText('translateY(-100%)');
+        setVisibleChapterText('hidden');
       }
 
       //checks if the site has loaded all necessary data and if not rerun the function after 500ms.
@@ -319,44 +319,28 @@ const Video = ({ loadUser, loading, user }) => {
 
   return (
     <>
-      <div className={styles.videocontainer}>
-        <div className={styles.videocontainer}>
-          {session.name !== 'dummy' ? (
-            <div key={session.tan}>
-              <h2> {session.name} </h2>
-              <video
-                // controls
-                // onProgress={(e) => pauseSegment(e)}
-                onTimeUpdate={updatePlayer}
-                ref={videoRef}
-                id='video-viviplayer'
-                //controls
-                preload='auto'
-                data-setup='{"fluid":true}' //This is used so that the video player is responsive
-                className='video-js vjs-default-skin vjs-big-play-centered'
-                onClick={togglePlayPause}
-              >
-                <source src={VIDEO_PREFIX + session.video_path} type='video/mp4' />
-              </video>
-            </div>
-          ) : (
-            <div key={session.tan}>
-              <video
-                // onProgress={(e) => pauseSegment(e)}
-                onTimeUpdate={updatePlayer}
-                ref={videoRef}
-                id='video-viviplayer'
-                //controls
-                preload='auto'
-                data-setup='{"fluid":true}' //This is used so that the video player is responsive
-                className='video-js vjs-default-skin vjs-big-play-centered'
-                onClick={togglePlayPause}
-              >
-                <source src={VIDEO_PREFIX + session.video_path} type='video/mp4' />
-              </video>
-            </div>
-          )}
-          <div className={styles.chapterinfocontainer} style={{ transform: visibleChapterText }}>
+      {session.name !== 'dummy' ? (<h2> {session.name} </h2>):("")}
+          <div className={styles.videocontainer}>
+            <div className={styles.videocontainer}>
+              
+                <div key={session.tan}>
+                  
+                  <video
+                    // controls
+                    // onProgress={(e) => pauseSegment(e)}
+                    onTimeUpdate={updatePlayer}
+                    ref={videoRef}
+                    id="video-viviplayer"
+                    //controls
+                    preload="auto"
+                    data-setup='{"fluid":true}' //This is used so that the video player is responsive
+                    className="video-js vjs-default-skin vjs-big-play-centered"
+                    onClick={togglePlayPause}
+                  >
+                    <source src={VIDEO_PREFIX + session.video_path} type="video/mp4" />
+                  </video>
+                </div>
+          <div className={styles.chapterinfocontainer} style={{ visibility: visibleChapterText }}>
             <p className={styles.chapterinfo}> {chapterText}</p>
           </div>
           {user != null && user.is_mod == true ? (
@@ -397,7 +381,16 @@ const Video = ({ loadUser, loading, user }) => {
               </div>
             </div>
           ) : (
-            ''
+            <div className={styles.controls}>
+              <div className={styles.progressbarcontainer}>
+                <div
+                  className={styles.progressbar}
+                  id='progressbar'
+                  style={{ width: progressBarWidth }}
+                ></div>
+                {markers}
+              </div>
+              </div>
           )}
         </div>
       </div>
