3095347a854b7e0e2f7da051cd1c1b658b31af50
The Anh Tran <theanh0226@gmail.com>
2022-01-12 22:56:49 +0100

    survey reply

---
+++ viviplayer-frontend/components/survey/SurveyRep.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import * as Survey from 'survey-react';
+
+const SurveyRep = ({ askFromAdminState }) => {
+  const [ask, setAsk] = useState(null);
+  useEffect(() => {
+    setAsk(askFromAdminState);
+  }, [askFromAdminState]);
+  const json = {
+    pages: [
+      {
+        // title: ask != null ? ask.title : '',
+        elements: [
+          {
+            type: ask != null ? ask.type : '',
+            name: 'answer',
+            title: ask != null ? ask.title : '',
+            isRequired: true,
+            choices: ask != null ? ask.answer : ''
+          }
+        ]
+      }
+    ]
+  };
+  const survey = new Survey.Model(json);
+  survey.onComplete.add((sender) => {
+    console.log(sender.data);
+  });
+
+  return <>{ask != null ? <Survey.Survey model={survey} completeText="Send" /> : ''}</>;
+};
+
+SurveyRep.propTypes = {};
+
+export default SurveyRep;
