cdf02a81c5cb02c26e4c1261a27690735a78ffc0
dinnguyen1495 <minhdinhnguyen1495@gmail.com>
2022-01-17 20:48:54 +0100

    change permission of GetStatisticsAPI

--- viviplayer/session/api/views.py
+++ viviplayer/session/api/views.py
@@ -25,31 +25,27 @@ from .serializers import (
     SentenceSerializer,
     QuestionSerializer,
     ShotSerializer,
-    AnswerSerializer
+    AnswerSerializer,
 )
 
 
 def tan_generator():
-    pwd_chars = [random.choice(string.ascii_lowercase + string.ascii_uppercase) for _ in range(random.randint(5, 6))]
+    pwd_chars = [
+        random.choice(string.ascii_lowercase + string.ascii_uppercase)
+        for _ in range(random.randint(5, 6))
+    ]
     pwd_digits = [random.choice(string.digits) for _ in range(random.randint(0, 2))]
-    special_chars = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-    pwd_special_chars = [random.choice(special_chars) for _ in range(random.randint(1, 2))]
+    special_chars = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+    pwd_special_chars = [
+        random.choice(special_chars) for _ in range(random.randint(1, 2))
+    ]
     pwd_list = pwd_chars + pwd_digits + pwd_special_chars
     random.shuffle(pwd_list)
     return "".join(pwd_list)
 
 
 def get_error_message(field_text, message_text):
-    msg = {
-        "errors": [
-            {
-                "field": field_text,
-                "message": [
-                    message_text
-                ]
-            }
-        ]
-    }
+    msg = {"errors": [{"field": field_text, "message": [message_text]}]}
     return msg
 
 
@@ -58,10 +54,10 @@ class SessionViewSet(viewsets.ModelViewSet):
     serializer_class = SessionSerializer
     queryset = ViViSession.objects.all()
     parser_classes = [FormParser, MultiPartParser]
-    http_method_names = ['get', 'post', 'put', 'delete']
+    http_method_names = ["get", "post", "put", "delete"]
 
     def get_permissions(self):
-        if self.action == 'list' or self.action == 'retrieve':
+        if self.action == "list" or self.action == "retrieve":
             permission_classes = [IsAuthenticated]
         else:
             permission_classes = [IsModerator]
@@ -72,7 +68,10 @@ class SessionViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         if ViViSession.objects.count() > 0:
-            error = get_error_message("non_field_errors", "Es daft nur maximal ein Session gleichzeitig geben!")
+            error = get_error_message(
+                "non_field_errors",
+                "Es daft nur maximal ein Session gleichzeitig geben!",
+            )
             return Response(data=error, status=status.HTTP_403_FORBIDDEN)
         return super().create(request, *args, **kwargs)
 
@@ -84,14 +83,14 @@ class SessionViewSet(viewsets.ModelViewSet):
 class ShotViewSet(viewsets.ModelViewSet):
     serializer_class = ShotSerializer
     queryset = Shot.objects.all()
-    http_method_names = ['get', 'post', 'put', 'delete']
+    http_method_names = ["get", "post", "put", "delete"]
 
     class Meta:
         model = Shot
-        fields = '__all__'
+        fields = "__all__"
 
     def get_permissions(self):
-        if self.action == 'list' or self.action == 'retrieve':
+        if self.action == "list" or self.action == "retrieve":
             permission_classes = [IsAuthenticated]
         else:
             permission_classes = [IsModerator]
@@ -103,8 +102,12 @@ class ShotViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         for shot in Shot.objects.all():
             if shot.time == float(request.data["time"]):
-                error = get_error_message("time", "Es gibt schon ein Shot mit dem gleichen Zeitstempel!")
-                return Response(data=error, status=status.HTTP_403_FORBIDDEN, headers={})
+                error = get_error_message(
+                    "time", "Es gibt schon ein Shot mit dem gleichen Zeitstempel!"
+                )
+                return Response(
+                    data=error, status=status.HTTP_403_FORBIDDEN, headers={}
+                )
         return super().create(request, *args, **kwargs)
 
 
@@ -112,7 +115,7 @@ class UserStoryViewSet(viewsets.ModelViewSet):
     serializer_class = UserStorySerializer
     queryset = UserStory.objects.all()
     permission_classes = [IsAuthenticated]
-    http_method_names = ['get', 'post', 'put', 'delete']
+    http_method_names = ["get", "post", "put", "delete"]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, session=ViViSession.objects.first())
@@ -122,7 +125,7 @@ class SentenceViewSet(viewsets.ModelViewSet):
     serializer_class = SentenceSerializer
     queryset = Sentence.objects.all()
     permission_classes = [IsAuthenticated]
-    http_method_names = ['get', 'post', 'put', 'delete']
+    http_method_names = ["get", "post", "put", "delete"]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, session=ViViSession.objects.first())
@@ -132,17 +135,19 @@ class QuestionViewSet(viewsets.ModelViewSet):
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
 
-    http_method_names = ['get', 'post', 'put', 'delete']
+    http_method_names = ["get", "post", "put", "delete"]
 
     def get_permissions(self):
-        if self.action == 'list' or self.action == 'retrieve':
+        if self.action == "list" or self.action == "retrieve":
             permission_classes = [IsAuthenticated]
         else:
             permission_classes = [IsModerator]
         return [permission() for permission in permission_classes]
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user, session=ViViSession.objects.first(), answers=[])
+        serializer.save(
+            author=self.request.user, session=ViViSession.objects.first(), answers=[]
+        )
 
 
 # API View for download a session as a .odt file
@@ -159,19 +164,33 @@ class ExportODT(APIView):
     textdoc.automaticstyles.addElement(withbreak)
 
     pstyle = Style(name="Paragraph 1", family="paragraph", defaultoutlinelevel="1")
-    pstyle.addElement(TextProperties(attributes={'fontsize': "10pt", 'fontfamily': "Arial"}))
+    pstyle.addElement(
+        TextProperties(attributes={"fontsize": "10pt", "fontfamily": "Arial"})
+    )
     textdoc.automaticstyles.addElement(pstyle)
 
     h1style = Style(name="Heading 1", family="paragraph", defaultoutlinelevel="1")
-    h1style.addElement(TextProperties(attributes={'fontsize': "20pt", 'fontweight': "bold", 'fontfamily': "Arial"}))
+    h1style.addElement(
+        TextProperties(
+            attributes={"fontsize": "20pt", "fontweight": "bold", "fontfamily": "Arial"}
+        )
+    )
     textdoc.automaticstyles.addElement(h1style)
 
     h2style = Style(name="Heading 2", family="paragraph", defaultoutlinelevel="1")
-    h2style.addElement(TextProperties(attributes={'fontsize': "16pt", 'fontweight': "bold", 'fontfamily': "Arial"}))
+    h2style.addElement(
+        TextProperties(
+            attributes={"fontsize": "16pt", "fontweight": "bold", "fontfamily": "Arial"}
+        )
+    )
     textdoc.automaticstyles.addElement(h2style)
 
     h3style = Style(name="Heading 3", family="paragraph", defaultoutlinelevel="1")
-    h3style.addElement(TextProperties(attributes={'fontsize': "12pt", 'fontweight': "bold", 'fontfamily': "Arial"}))
+    h3style.addElement(
+        TextProperties(
+            attributes={"fontsize": "12pt", "fontweight": "bold", "fontfamily": "Arial"}
+        )
+    )
     textdoc.automaticstyles.addElement(h3style)
 
     def get(self, request, format=None):
@@ -190,7 +209,13 @@ class ExportODT(APIView):
             self.textdoc.text.addElement(p)
             self.textdoc.text.addElement(P())
             href = self.textdoc.addPicture(s.image)
-            photoframe = Frame(width="300pt", height="200pt", x="56pt", y="56pt", anchortype="paragraph")
+            photoframe = Frame(
+                width="300pt",
+                height="200pt",
+                x="56pt",
+                y="56pt",
+                anchortype="paragraph",
+            )
             photoframe.addElement(Image(href=href))
             p.addElement(photoframe)
 
@@ -200,7 +225,9 @@ class ExportODT(APIView):
             self.textdoc.text.addElement(P())
 
             for (j, us) in enumerate(UserStory.objects.filter(shot=s)):
-                userstorytext = us.damit + ' ' + us.moechteichals1 + ' ' + us.moechteichals2
+                userstorytext = (
+                    us.damit + " " + us.moechteichals1 + " " + us.moechteichals2
+                )
                 p = P(text=userstorytext, stylename=self.pstyle)
                 self.textdoc.text.addElement(p)
                 self.textdoc.text.addElement(P())
@@ -224,20 +251,24 @@ class ExportODT(APIView):
                 self.textdoc.text.addElement(P(text=q.title, stylename=self.pstyle))
 
                 # Add Answers for Question
-                self.textdoc.text.addElement(P(text=f'- Choices: {", ".join(q.choices)}', stylename=self.pstyle))
+                self.textdoc.text.addElement(
+                    P(text=f'- Choices: {", ".join(q.choices)}', stylename=self.pstyle)
+                )
 
             # Add page break
             p = P(stylename=self.withbreak)
             self.textdoc.text.addElement(p)
 
         # Save document to server
-        self.textdoc.save('media/' + str(ses.id) + '.odt')
+        self.textdoc.save("media/" + str(ses.id) + ".odt")
 
         # Return document
-        data = open('media/' + str(ses.id) + '.odt', 'rb')
-        response = HttpResponse(data, content_type='application/vnd.oasis.opendocument.text')
-        response['Content-Disposition'] = 'attachment; filename="export.odt"'
-        response['Content-Length'] = os.path.getsize('media/' + str(ses.id) + '.odt')
+        data = open("media/" + str(ses.id) + ".odt", "rb")
+        response = HttpResponse(
+            data, content_type="application/vnd.oasis.opendocument.text"
+        )
+        response["Content-Disposition"] = 'attachment; filename="export.odt"'
+        response["Content-Length"] = os.path.getsize("media/" + str(ses.id) + ".odt")
         return response
 
 
@@ -254,7 +285,7 @@ class ExportCSV(APIView):
         # Add Screenshots to .zip
         buffer = io.BytesIO()
         response = zipfile.ZipFile(buffer, "w")
-        for dirname, subdirs, files in os.walk('media/screenshots/' + str(ses.id)):
+        for dirname, subdirs, files in os.walk("media/screenshots/" + str(ses.id)):
             for filename in files:
                 response.write(os.path.join(dirname, filename), filename)
 
@@ -266,12 +297,12 @@ class ExportCSV(APIView):
             row = [
                 f"User Story {i + 1}",
                 f"{us.damit} {us.moechteichals1} {us.moechteichals2}",
-                str(us.shot.image).split("/")[-1]
+                str(us.shot.image).split("/")[-1],
             ]
             writer.writerow(row)
 
         # Add .csv to .zip
-        response.writestr('export.csv', csvf.getvalue())
+        response.writestr("export.csv", csvf.getvalue())
         response.close()
 
         # Save .zip on server - If needed
@@ -281,8 +312,8 @@ class ExportCSV(APIView):
 
         # Return .zip in response
         response = HttpResponse(buffer.getvalue())
-        response['Content-type'] = 'application/x-zip-compressed'
-        response['Content-Disposition'] = 'attachment; filename="export.zip"'
+        response["Content-type"] = "application/x-zip-compressed"
+        response["Content-Disposition"] = 'attachment; filename="export.zip"'
 
         return response
 
@@ -295,11 +326,13 @@ class PostAnswerAPIView(generics.CreateAPIView):
     def post(self, request, *args, **kwargs):
         serializer = AnswerSerializer(data=request.data)
         serializer.is_valid()
-        question_id = serializer.data['question_id']
-        answer = serializer.data['answer']
+        question_id = serializer.data["question_id"]
+        answer = serializer.data["answer"]
 
         if Question.objects.filter(id=question_id).count() == 0:
-            msg = get_error_message("question_id", "Frage/Umfrage kann nicht gefunden werden!")
+            msg = get_error_message(
+                "question_id", "Frage/Umfrage kann nicht gefunden werden!"
+            )
             return Response(data=msg, status=status.HTTP_404_NOT_FOUND)
 
         if len(answer) == 0:
@@ -309,24 +342,20 @@ class PostAnswerAPIView(generics.CreateAPIView):
         question = Question.objects.get(id=question_id)
         for choice in answer:
             if choice not in question.choices:
-                msg = get_error_message("answer", f"Ungültige Antwort für die eingegebene Frage/Umfrage")
+                msg = get_error_message(
+                    "answer", f"Ungültige Antwort für die eingegebene Frage/Umfrage"
+                )
                 return Response(data=msg, status=status.HTTP_400_BAD_REQUEST)
 
         question.answers += answer
         question.save()
-        data = {
-            "success": {
-                "message": [
-                    "Successfully sent the answer!"
-                ]
-            }
-        }
+        data = {"success": {"message": ["Successfully sent the answer!"]}}
         return Response(data=data, status=status.HTTP_200_OK)
 
 
 # API for moderator to get statistics data
 class GetStatisticsAPIView(APIView):
-    permission_classes = [IsModerator]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         if "question_id" not in self.kwargs:
@@ -336,7 +365,9 @@ class GetStatisticsAPIView(APIView):
         question_id = self.kwargs.get("question_id")
 
         if Question.objects.filter(id=question_id).count() == 0:
-            msg = get_error_message("question_id", "Frage/Umfrage kann nicht gefunden werden!")
+            msg = get_error_message(
+                "question_id", "Frage/Umfrage kann nicht gefunden werden!"
+            )
             return Response(data=msg, status=status.HTTP_404_NOT_FOUND)
 
         statistics = []
@@ -349,6 +380,6 @@ class GetStatisticsAPIView(APIView):
         data = {
             "question_id": question_id,
             "question_title": question.title,
-            "data": statistics
+            "data": statistics,
         }
         return Response(data=data, status=status.HTTP_200_OK)
