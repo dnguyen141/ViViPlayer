91faf5d59f769b4d17eda52503650eaec530bd46
jedi <git@m.j3d1.de>
2022-01-17 15:07:25 +0100

    using environment variables to switch between development and production mode

--- viviplayer-frontend/constants/constants.js
+++ viviplayer-frontend/constants/constants.js
@@ -1,3 +1,3 @@
-export const SERVER_BACKEND = '';
-export const WS_BACKEND = '';
+export const SERVER_BACKEND = process.env.VIVIMODE==='prod' ? '' : 'http://127.0.0.1:8000';
+export const WS_BACKEND = process.env.VIVIMODE==='prod' ? '' : 'ws://127.0.0.1:8000';
 export const VIDEO_PREFIX = SERVER_BACKEND + '/media/';
