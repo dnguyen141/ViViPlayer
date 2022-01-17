916459197b1f1cead39f6c18395a517b57232317
The Anh Tran <theanh0226@gmail.com>
2022-01-17 22:10:06 +0100

    get QuestionId form Moderator

--- viviplayer-frontend/components/user-story/UserStory.js
+++ viviplayer-frontend/components/user-story/UserStory.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Divider } from 'antd';
 import PropTypes from 'prop-types';
 import Satz from '../satz/Satz';
 import UsrStoryDesire from './UsrStoryDesire';
 import SurveyContainer from '../survey/SurveyContainer';
-
+import { VIDEO_PREFIX, WS_BACKEND } from '../../constants/constants';
+let socket;
 const tabList = [
   {
     key: 'tab1',
@@ -20,7 +21,18 @@ const tabList = [
   }
 ];
 
-const UserStory = ({currentShot}) => {
+const UserStory = ({ currentShot }) => {
+  // connect to socket and update sentence table
+  useEffect(() => {
+    const url = (WS_BACKEND || 'ws://' + window.location.host) + '/ws/player/sessionid12345/';
+    socket = new WebSocket(url);
+    socket.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+      if (data.action === 'questionFromServer') {
+        localStorage.setItem('questionID', data.payload.id);
+      }
+    };
+  }, []);
   const [activeTabKey1, setActiveTabKey1] = useState('tab1');
   const onTab1Change = (key) => {
     setActiveTabKey1(key);
@@ -29,12 +41,12 @@ const UserStory = ({currentShot}) => {
   const contentList = {
     tab1: (
       <Card style={{ border: '3px solid gray' }}>
-        <UsrStoryDesire currentShot={currentShot}/>
+        <UsrStoryDesire currentShot={currentShot} />
       </Card>
     ),
     tab2: (
       <Card style={{ border: '3px solid gray' }}>
-        <Satz currentShot={currentShot}/>
+        <Satz currentShot={currentShot} />
       </Card>
     ),
     tab3: (
