de7171374e18e11ce9c36d52d7019d8389ed0dd2
Tim <tim.bohlmann@stud.uni-hannover.de>
2022-01-19 01:51:10 +0100

    finished utils

--- viviplayer-frontend/utils/api.js
+++ viviplayer-frontend/utils/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { SERVER_BACKEND } from '../constants/constants';
 // Create an instance of axios
+
+/**
+ * Api shortcut to an axios request.
+ */
 const api = axios.create({
   baseURL: `${SERVER_BACKEND}/api`,
   headers: {
--- viviplayer-frontend/utils/notification.js
+++ viviplayer-frontend/utils/notification.js
@@ -1,5 +1,11 @@
 import { notification } from 'antd';
 
+/**
+ * Sends a notification to the screen.
+ * @param {string} mess The Title of the notification.
+ * @param {*} noti The description of the notification.
+ * @param {*} type Type of notfication.
+ */
 export const Notification = (mess, noti, type) => {
   notification[type]({
     message: mess,
