698289f837d37bfa6329d1d9f9a05580d0d3eb42
jedi <git@m.j3d1.de>
2022-01-19 07:14:00 +0100

    move django admin interface

--- viviplayer/config/urls.py
+++ viviplayer/config/urls.py
@@ -39,7 +39,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path("api/auth/", include("authentication.api.urls")),
     path("api/session/", include("session.api.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
