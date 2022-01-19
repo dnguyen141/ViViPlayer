2c91405fa67d47baad092576f60e435a3570c81e
Tim <tim.bohlmann@stud.uni-hannover.de>
2022-01-19 02:45:57 +0100

    finished reducers and store

--- viviplayer-frontend/reducers/index.js
+++ viviplayer-frontend/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import session from './session';
-
+/**
+ * Root Reducer combining both Reducers.
+ */
 const rootReducer = combineReducers({
   auth,
   session
